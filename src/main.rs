8644bd782b0252164fb5be4a1e38697ccbd169a1
Eric Huss <eric@huss.org>
2017-02-01 18:00:59 -0800

    Chapter 12: Where next?

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,38 @@ fn color<T: Hitable>(rng: &mut Rng, r: &Ray<f64>, world: &T, depth: u8) -> Vec3<
     }
 }
 
-// Camera is at 0,0,0
+fn random_scene<'a>(rng: &mut Rng) -> HitableList<'a> {
+    let mut list = HitableList::new();
+    list.add_hitable(Sphere::new(Vec3::new(0.0, -1000.0, 0.0), 1000.0, Rc::new(Lambertian::new(Vec3::new(0.5, 0.5, 0.5)))));
+    // XXX: Not sure why explicit u8 is required here to cast to f64.
+    for a in -11..11i8 {
+        for b in -11..11i8 {
+            let choose_mat = rng.rand64();
+            let center = Vec3::new(a as f64 + 0.9 * rng.rand64(), 0.2, b as f64+0.9*rng.rand64());
+            if (center-Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 { //diffuse
+                    list.add_hitable(
+                        Sphere::new(center, 0.2, Rc::new(Lambertian::new(
+                            Vec3::new(rng.rand64()*rng.rand64(), rng.rand64()*rng.rand64(), rng.rand64()*rng.rand64())))));
+                } else if choose_mat < 0.95 { //metal
+                    list.add_hitable(
+                        Sphere::new(center, 0.2, Rc::new(Metal::new(
+                            Vec3::new(0.5*(1.0 + rng.rand64()),
+                                      0.5*(1.0 + rng.rand64()),
+                                      0.5*(1.0 + rng.rand64())), 0.5*rng.rand64()))));
+                } else { //glass
+                    list.add_hitable(Sphere::new(center, 0.2, Rc::new(Dielectric::new(1.5))));
+                }
+            }
+        }
+    }
+    list.add_hitable(Sphere::new(Vec3::new(0.0, 1.0, 0.0), 1.0, Rc::new(Dielectric::new(1.5))));
+    list.add_hitable(Sphere::new(Vec3::new(-4.0, 1.0, 0.0), 1.0, Rc::new(Lambertian::new(Vec3::new(0.4, 0.2, 0.1)))));
+    list.add_hitable(Sphere::new(Vec3::new(4.0, 1.0, 0.0), 1.0, Rc::new(Metal::new(Vec3::new(0.7, 0.6, 0.5), 0.0))));
+    return list;
+}
+
+
 // y-up
 // into screen is -z
 // Traverse from lower-left corner (-2, -1) to upper-right +(+2,+2)
@@ -35,19 +66,13 @@ fn main() {
     let ny = 200;
     let ns = 100;
     println!("P3\n{} {}\n255", nx, ny);
-    let lookfrom = Vec3::new(3.0, 3.0, 2.0);
-    let lookat = Vec3::new(0.0, 0.0, -1.0);
-    let dist_to_focus = (lookfrom-lookat).length();
-    let aperture = 2.0;
-    let cam = Camera::new(lookfrom, lookat, Vec3::new(0.0, 1.0, 0.0), 20.0, nx as f64/ny as f64, aperture, dist_to_focus);
-    let mut world = HitableList::new();
-    // Small sphere.
-    world.add_hitable(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, Rc::new(Lambertian::new(Vec3::new(0.1, 0.2, 0.5)))));
-    // Really huge sphere (covers bottom of screen).
-    world.add_hitable(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, Rc::new(Lambertian::new(Vec3::new(0.8, 0.8, 0.0)))));
-    world.add_hitable(Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, Rc::new(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.2))));
-    world.add_hitable(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, Rc::new(Dielectric::new(1.5))));
     let mut rng = Rng::new();
+    let lookfrom = Vec3::new(13.0, 2.0, 3.0);
+    let lookat = Vec3::new(0.0, 0.0, 0.0);
+    let dist_to_focus = 10.0;//(lookfrom-lookat).length();
+    let aperture = 0.1;
+    let cam = Camera::new(lookfrom, lookat, Vec3::new(0.0, 1.0, 0.0), 20.0, nx as f64/ny as f64, aperture, dist_to_focus);
+    let world = random_scene(&mut rng);
     for j in (0..ny - 1).rev() {
         for i in 0..nx {
             let mut col = Vec3::<f64>::zero();
